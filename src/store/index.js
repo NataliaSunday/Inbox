540720ba799e1f7f9ac2cbd83e1959b82286cbcd
unknown <n.niedziela201@gmail.com>
2022-10-11 17:22:30 +0200

    fix eslint --fix error

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,34 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    emails: [
+      {
+        id: 1,
+        tittle: "Lorem ipsum",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+        isRead: false,
+        isArchive: false,
+      },
+      {
+        id: 1,
+        tittle: "Dolor Sit",
+        content:
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        isRead: false,
+        isArchive: false,
+      },
+      {
+        id: 1,
+        tittle: "At vero eos et accusamus et iusto...",
+        content:
+          "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+        isRead: true,
+        isArchive: false,
+      },
+    ],
+  },
   getters: {},
   mutations: {},
   actions: {},
